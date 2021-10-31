9a4dcd79d21fe27104bae23ba661f1c3818f76a0
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-10-31 13:14:25 +0700

    Add Header, Footer, change CSS in flie App

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import DetailDish from "./DetailDishes";
 import { DISHES } from "../shared/dishes"
 
@@ -22,19 +24,14 @@ class Main extends Component {
   render() {
     return (
       <div>
-            <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                </div>
-            </Navbar>
-            
-            <Menu dishes = {this.state.dishes} onClick={(dishId) => this.handleClick(dishId)}/>
-            <div className="container">
-                <div className="row">
-                    <DetailDish dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish) [0]}/>
-                </div>
-
+        <Header/>
+        <Menu dishes = {this.state.dishes} onClick={(dishId) => this.handleClick(dishId)}/>
+        <div className="container">
+            <div className="row">
+                <DetailDish dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish) [0]}/>
             </div>
+          </div>
+          <Footer/>
       </div>
     )
   };
