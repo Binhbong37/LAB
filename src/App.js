44d14b580ac7b729fd3cac257b2998e96af584ea
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-11-01 12:23:32 +0700

    Introduce Redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Main from './Components/MainComponent';
 import { Component } from 'react';
 import {BrowserRouter} from "react-router-dom"
+import { Provider } from "react-redux";
+import { configureStore } from "./redux/configureStore"
+
 class App extends Component {
- 
   render() {
+
+    const store = configureStore()
     return (
-      <BrowserRouter>
-        <div>
-          <Main/>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main/>
+          </div>
+        </BrowserRouter>
+      </Provider>
     )
   };
 }
